0cde09aaf7f6798e9002c02f48ead6d35ad7189d
David <iq.davidh@gmail.com>
2019-04-27 15:54:34 -0500

    no se porque no los agreaggo

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
+
+function crearPublic() {
+    console.log("crear public");
+    $.livereload.listen();
+
+    return gulp
+        .src("./public/**.*")
+        .pipe($.livereload())
+        ;
+}
+
+
+function validar() {
+    return gulp
+        .src("./public/index.html")
+        .pipe($.w3cHtmlValidator())
+        .pipe($.w3cHtmlValidator.reporter())
+        ;
+}
+
+
+gulp.task("watch", () => {
+    gulp.watch("./public/**.*", crearPublic)
+});
+
+gulp.task("validar", validar);
