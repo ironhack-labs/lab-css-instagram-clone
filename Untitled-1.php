aaa6d3ea786253564b3532a1056d246252177d7c
Carlota Trias <carlotatriasame@gmail.com>
2023-10-10 09:15:13 +0200

    done

---
+++ Untitled-1.php
@@ -0,0 +1,15 @@
+    <h1> Instagram</h1> 
+    <p>Capture and Share the World's Moments</p> 
+    <img src="./images/phones.png" alt="two fones showing Instagram's home page">
+   
+    <p>Instagram is a fast, beautiful and fun way to share your life with
+    friends and family.</p>
+
+    <p>Take a picture or video, choose a filter to transform its look and feel, then post to Instagram
+    &mdash; it's that easy. You can even share to Facebook, Twitter, Tumblr and more. It's a new way to see the world.</p>
+
+    <p>Oh yeah, did we mention it's free?</p> 
+    
+    <p>Download on the App Store</p>
+    <p>Get it on Google Play </p>  
+    <p>About Us SupportBlog Press API Jobs Privacy Terms &copy; 2014 Instagram</p>
